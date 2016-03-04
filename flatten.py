492344215e8e879a20f6387c8602ff8cb5a06c21
Vincent QB <vincentqb@users.noreply.github.com>
2016-03-04 17:27:49 -0500

    expans more.

--- flatten.py
+++ flatten.py
@@ -1,5 +1,6 @@
-test = {'a': 1, 'c': {'a': 2, 'b': {'x': 5, 'y' : 10}}, 'd': [1, 2, 3], 'e':[{'x':1}, {'y':2}]}
+test = {'a': 1, 'c': {'a': 2, 'b': {'x': 5, 'y' : 10}}, 'd': [1, 2, 3], 'e':[{'x':1}, {'y':2}, {'x':2}], 'f': [1, 2]}
 
+# http://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
 def flatten_dict(d, parent_key = None):
     if parent_key is None:
         parent_key = ''
@@ -7,12 +8,17 @@ def flatten_dict(d, parent_key = None):
     for k, v in d.items():
         try:
             if (type(v)==type([])): 
-                for l in v: 
-                    items.extend(flatten_dict(l, '%s%s_' % (parent_key, k)).items())
+                for l,w in enumerate(v): 
+                    items.extend(flatten_dict(w, str(parent_key) + str(k) + '_' + str(l) + '_').items())
             else: 
                 items.extend(flatten_dict(v).items())
         except AttributeError:
-            items.append(('%s%s' % (parent_key, k), v))
+            items.append((str(parent_key) + str(k), v))
     return dict(items)
 
-print(flatten_dict(test))
+f = flatten_dict(test)
+print(f)
+
+import pandas as pd
+p = pd.DataFrame(f)
+print(p)
